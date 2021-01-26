94bb52c5fcc549cfb8817d07aac837c3eb46aa79
Conrad Rosenbrock <rosenbrockc@gmail.com>
2021-01-25 21:54:23 -0700

    Initial package for branch/commit working okay.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Git Brancher and Committer
+
+This is a simple-minded, tiny package that just creates abstracting functions
+for creating new branches for a repo and submodules, as well as committing
+changes automatically in python.
+
+```
+pip install giter
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+    args = {}
+except ImportError:
+    from distutils.core import setup
+    print("""\
+*** WARNING: setuptools is not found.  Using distutils...
+""")
+
+from setuptools import setup
+setup(name='giter',
+      version='0.0.1',
+      description='Wrapper functions to help with branches and submodules in `happyai`.',
+      author='Happy Health',
+      author_email='developer@happy.ai',
+      url='',
+      #license='GPL-3.0',
+      packages=['giter'],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Natural Language :: English',
+          'Operating System :: MacOS',
+          'Operating System :: Unix',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.7'
+      ],
+     )
